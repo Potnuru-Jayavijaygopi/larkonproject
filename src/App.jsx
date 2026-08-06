e50d120502f842676f0855fed563d4630d2daf54
Potnuru-Jayavijaygopi <ponturu@paithacs.com>
2026-08-06 12:36:17 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
+
 import Dashboard from './pages/Dashboard';
 
 import ProductList from './pages/product/ProductList';
@@ -8,6 +9,14 @@ import ProductGrid from './pages/product/ProductGrid';
 import ProductDetails from './pages/product/ProductDetails';
 import AddProduct from './pages/product/AddProduct';
 import CreateProduct from './pages/product/CreateProduct';
+
+
+import ComingSoon from './pages/pages1/ComingSoon';
+import Maintenance from './pages/pages1/Maintenance';
+import Error404 from './pages/pages1/Error404';
+import Timeline from './pages/pages1/Timeline';
+import Pricing from './pages/pages1/Pricing';
+
 
 
 
@@ -25,9 +34,17 @@ const router = createBrowserRouter([
       { path: 'products/add', element: <AddProduct /> },
       { path: 'products/create', element: <CreateProduct /> },
 
+      { path: 'pages/coming-soon', element: <ComingSoon /> },
+      { path: 'pages/maintenance', element: <Maintenance /> },
+      { path: 'pages/error-404', element: <Error404 /> },
+      { path: 'pages/timeline', element: <Timeline /> },
+      { path: 'pages/pricing', element: <Pricing /> },
+
+
       { path: '*', element: <Dashboard /> },
     ],
   },
+  
 ]);
 
 export default router;
