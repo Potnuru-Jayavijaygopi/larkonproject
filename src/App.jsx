b6ddf29499beb7cb5f46ff45b9b16bab2932209a
priya <morthala@paithacs.com>
2026-08-06 14:53:29 +0530

    added calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
+
 import Dashboard from './pages/Dashboard';
 
-import ProductList from './pages/product/ProductList';
-import ProductGrid from './pages/product/ProductGrid';
-import ProductDetails from './pages/product/ProductDetails';
-import AddProduct from './pages/product/AddProduct';
-import CreateProduct from './pages/product/CreateProduct';
+
+
+
+
+
 
 
 
@@ -19,15 +20,13 @@ const router = createBrowserRouter([
       { index: true, element: <Dashboard /> },
       { path: 'dashboard', element: <Dashboard /> },
 
-      { path: 'products/list', element: <ProductList /> },
-      { path: 'products/grid', element: <ProductGrid /> },
-      { path: 'products/details', element: <ProductDetails /> },
-      { path: 'products/add', element: <AddProduct /> },
-      { path: 'products/create', element: <CreateProduct /> },
+      
+
 
       { path: '*', element: <Dashboard /> },
     ],
   },
+  
 ]);
 
 export default router;
